54625814daf0d6abfde50efe62b530c72dd7034a
ChiritaAlexandru <alexandru.chirita93@gmail.com>
2022-01-12 14:58:48 +0200

    Update Dog.java

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -6,8 +6,9 @@ public class Dog {
 
     public void addNume(String nume){
         //System.out.println(nume);
-        Stream.of("1","2","3").mapToInt(x -> Integer.parseInt(x))
-                .forEach( c -> System.out.println(c));
+        Stream.of("1","2","3")
+            .mapToInt(x -> Integer.parseInt(x))
+            .forEach( c -> System.out.println(c));
         System.out.println("test");
     }
     
