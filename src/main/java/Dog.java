f1f94f42a5ca35b93641ad47fa7632012fe5f273
Alex <alexandru.chirita93@gmail.com>
2022-01-12 20:18:03 +0200

    formatting Dog.class

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -4,18 +4,18 @@ public class Dog {
     private String nume;
 
 
-    public void addNume(String nume){
+    public void addNume(String nume) {
     /*    System.out.println();
         System.out.println(nume);
 
         //System.out.println(nume); */
-        Stream.of("1","2","3")
-            .mapToInt(x -> Integer.parseInt(x))
-            .forEach( c -> System.out.println(c));
+        Stream.of("1", "2", "3")
+                .mapToInt(x -> Integer.parseInt(x))
+                .forEach(c -> System.out.println(c));
         System.out.println("test");
     }
-    
-      public void showName(String nume){
+
+    public void showName(String nume) {
         System.out.println(nume);
     }
 }
