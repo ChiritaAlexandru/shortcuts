28f56b4160fbb03e4a52efba272a3793b8c95346
ChiritaAlexandru <alexandru.chirita93@gmail.com>
2022-01-12 15:14:18 +0200

    Update Dog.java

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -5,6 +5,7 @@ public class Dog {
     public void addNume(String nume){
         System.out.println();
         System.out.println(nume);
+        System.out.println("test");
     }
     
       public void showName(String nume){
