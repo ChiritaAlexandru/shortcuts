5c4d067c35df3159704a01895151758a5c5baf1f
Alex <alexandru.chirita93@gmail.com>
2022-05-10 23:25:04 +0300

    Mesaj2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String [] args){
@@ -7,5 +9,21 @@ public class Main {
         DateTimeFormatter formatter =DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
        // var d = LocalDateTime.parse(localDateTime.toString(),formatter);
         System.out.println(LocalDateTime.now().format(formatter));
+
+
+        try{
+            throw new RuntimeException("here");
+        }catch(RuntimeException re){
+            System.out.println( re.getLocalizedMessage());
+        }
+        List<String> kust = new ArrayList<>(4);
+        kust.add("1");
+        kust.add("2");
+
+        kust.forEach( i -> {
+            kust.add(new String("dd"));
+        });
+
+        System.out.println(kust.size());
     }
 }
--- src/main/java/chapter18/RunnableInterface.java
+++ src/main/java/chapter18/RunnableInterface.java
@@ -1,2 +1,41 @@
-package chapter18;public class RunnableInterface {
+package chapter18;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class RunnableInterface {
+    public static void main(String [] args) throws ExecutionException, InterruptedException {
+        Runnable task1 = () -> {
+            System.out.println("task 1");
+            return;
+        };
+        Runnable task2 = () -> {
+            System.out.println("task 2");
+        };
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        // method void execute(); executa task la un moment dat in viitor
+
+        System.out.println("begin");
+
+        System.out.println("begin");
+       // executorService.execute(task1);
+        //executorService.execute(task2);
+
+
+        // method Future<?> submit(Runnable r);
+
+        Future future =executorService.submit(task1);
+        System.out.println(future.get());
+    }
+
+    /*
+    @FunctionalInterface
+    public interface Runnable{
+
+        public void run(){}
+    }
+     */
 }
